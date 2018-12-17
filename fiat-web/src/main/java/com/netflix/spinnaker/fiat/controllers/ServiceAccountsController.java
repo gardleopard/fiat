5ef649561f01691a89a91a14d4c541f14261562a
Gard Rimestad <gard.rimestad@schibsted.com>
2018-12-17 12:30:36 +0100

    first iteration

---
+++ fiat-web/src/main/java/com/netflix/spinnaker/fiat/controllers/ServiceAccountsController.java
@@ -0,0 +1,39 @@
+package com.netflix.spinnaker.fiat.controllers;
+
+import com.netflix.spinnaker.fiat.model.resources.ServiceAccount;
+import com.netflix.spinnaker.fiat.providers.ResourceProvider;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import retrofit.http.Body;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/serviceAccounts")
+public class ServiceAccountsController {
+  private final ResourceProvider<ServiceAccount> serviceAccountResourceProvider;
+
+  public ServiceAccountsController(ResourceProvider<ServiceAccount> serviceAccountResourceProvider) {
+    this.serviceAccountResourceProvider = serviceAccountResourceProvider;
+  }
+  @RequestMapping(value = "/{serviceAccountName:.+}", method = RequestMethod.PUT)
+  public Map<String, String> updateApplication(@PathVariable String serviceAccountName, @Body @NonNull List<String> memberOf, HttpServletResponse response) {
+    ServiceAccount serviceAccount = new ServiceAccount();
+    serviceAccount.setName(serviceAccountName);
+    serviceAccount.setMemberOf(memberOf);
+    log.info("Updating serviceAccount {}", serviceAccountName);
+    //Application app = front50Service.getApplicationPermissions(applicationName.toLowerCase());
+    //      fiatService.createServiceAccount(serviceAccount.name, serviceAccount)
+
+    serviceAccountResourceProvider.addItem(serviceAccount);
+    return Collections.singletonMap("status", "success");
+  }
+}
